67d9512e9fb83ae2e10f05c2031b9d1f8ee63e84
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2023-10-10 18:25:48 -0600

    Add files via upload

---
+++ SheetsEditor.py
@@ -0,0 +1,27 @@
+# C:\Users\techn\Desktop\Coding\Python\pythonProjects\SheetsEditor\car-prices.csv
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+st.title("Sheets Editor")
+st.sidebar.title("Controls:")
+
+c1, c2, c3, c4, c5, c6 = st.columns(6)
+	
+method = c1.selectbox("Select input method:", ["local", "hyperlink"])
+link = ""
+mode = st.sidebar.radio("", ["Load/Reset Data", "Editor"])
+
+if method == "local":
+	link = link + st.text_input("Enter .csv file path here:")
+elif method == "hyperlink":
+	link = st.text_input("Enter .csv file link here:")
+
+st.write("\n\n---\n\n")
+
+c1, c2, c3, c4, c5, c6 = st.columns(6)
+
+df = pd.read_csv(link)
+
+st.experimental_data_editor(df)
